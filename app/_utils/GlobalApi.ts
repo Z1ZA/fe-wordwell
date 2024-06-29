3c03dc239f91ee7fc1ba133455a4d93d31661533
Abdul Aziz <sayaabdaziz1@gmail.com>
2024-06-29 20:28:28 +0700

    feat: connect to deployed strapi

--- app/_utils/GlobalApi.ts
+++ app/_utils/GlobalApi.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = "http://localhost:1337/api";
+// const apiUrl = "http://localhost:1337/api";
+const apiUrl = "https://mindful-books-eeb8cba6ed.strapiapp.com/api";
 
 const axiosClient: AxiosInstance = axios.create({
   baseURL: apiUrl,
@@ -10,8 +11,6 @@ const axiosClient: AxiosInstance = axios.create({
   },
 });
 
-// const getAllWords = (): Promise<AxiosResponse<Word[]>> =>
-//   axiosClient.get("/words");
 const getAllWords = () => axiosClient.get("/words?populate=*");
 
 export default { getAllWords };
